04454ac1b6a3ad1fff4af88717f6359b37c60bce
pdhende <pdhendework@gmail.com>
2022-02-01 00:36:42 -0500

    Updated JS file to include code to show future timeblocks in green

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,7 @@
-var timeBlockEl = $(".time-block-val");
+
+// Fetch elements
+var timeValEl = $(".time-block-val");
+var timeBlockEl = $(".time-block");
 
 // Display current day
 var currDay = moment().format('dddd') + ", " + moment().format('MMMM Do');
@@ -6,14 +9,20 @@ $('#currentDay').text(currDay);
 
 // Function to display the calendar based on the time of day
 function init() {
-    console.log(timeBlockEl);
-    console.log(timeBlockEl.length);
+    console.log(timeValEl);
+    console.log(timeValEl.length);
     var currHour = moment().hour();
-    // console.log(currHour);
-    for(var i = 0; i < timeBlockEl.length; i++) {
-        console.log(timeBlockEl[i].id);
+    console.log("Current Hour: "+currHour);
+    for(var i = 0; i < timeValEl.length; i++) {
+        var timeVal = timeValEl[i].id;
+        console.log("Past Time: " +timeVal);
+
+        // If the time is in future set background color of time block to green
+        if(timeVal > currHour) {
+            timeBlockEl.eq(i).addClass("future");
+        }
     }
 }
 
-
+// Function called when the page loads
 init();
