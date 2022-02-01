75b3139b049e9903abb5135175d32eba25df0606
pdhende <pdhendework@gmail.com>
2022-02-01 00:09:30 -0500

    Updated JS file to include code to get the id of the timebloacks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,19 @@
+var timeBlockEl = $(".time-block-val");
+
 // Display current day
 var currDay = moment().format('dddd') + ", " + moment().format('MMMM Do');
 $('#currentDay').text(currDay);
+
+// Function to display the calendar based on the time of day
+function init() {
+    console.log(timeBlockEl);
+    console.log(timeBlockEl.length);
+    var currHour = moment().hour();
+    // console.log(currHour);
+    for(var i = 0; i < timeBlockEl.length; i++) {
+        console.log(timeBlockEl[i].id);
+    }
+}
+
+
+init();
